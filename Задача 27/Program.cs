53362e513a9feab14a0397a547752d469a9fd574
Никита Янкко <nikita.yanko1996@gmail.com>
2023-02-10 20:13:39 +0300

    Сделал домашку 4

---
+++ Задача 27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число, и программа посчитает сумму цифр в числе");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum(int a)
+{
+int result = 0;
+    while (a>10)
+    {
+    result += a % 10;
+    a /= 10;
+    }
+    result += a;
+    return result;
+}
+
+int result = sum(a);
+Console.WriteLine($"{result}");
+
+
---
+++ Задача 29/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19, -5, 0, -11 -> [1, 2, 5, 7, 19, -5, 0, -11]
+
+
+void FillArray(int [] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random() .Next(-100, 100);
+        index++;
+    }
+
+}
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{(coll[position])}" + ",");
+        position++;
+    }
+}
+int [] array = new int [8];
+FillArray(array);
+PrintArray(array);
+
+
+
